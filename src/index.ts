e4e6060f8b88ceb9362101e1e20a6f14152e7e81
Salakhutdinov Salavat <salahutdinov.salavat@gmail.com>
2023-01-29 07:03:05 +0300

    feat: add implementation

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,129 @@
-const singleLineCommentsRegExp = /\/\/.*/gm;
-const multiLineCommentsRegExp = /\/\*(.|\n)*?\*\//gm;
-const trailingCommasRegExp = /(,)((?:\s|\n)*?(?:}|]))/gm;
-const unquotedPropertyRegExp = /(?<name>[a-zA-Z0-9]+)(\n|\s)*:/gm;
-const stringsRegExp = /(?<!\\)(?<quote>['"])(?:\\\k<quote>|.)*?\k<quote>/gm;
+const singleQuotesStringRegExp =
+  /(?<!\\)(?<singleQuotesString>'(?:\\'|[^'\n])*?')/;
 
-function unescapeDoubleQuotesString(string: string) {
-  return string.replace(/\\'/gm, "'");
+const doubleQuotesStringRegExp =
+  /(?<!\\)(?<doubleQuotesString>"(?:\\"|[^"\n])*?")/;
+
+const templateStringRegExp =
+  /(?<!\\)(?<templateString>`(?:\\`|(?:(?<!\\)\$\{.*?\})|[^`])*?`)/;
+
+const singleQuotesStringSBKeyRegExp = new RegExp(
+  `\\[\\s*${singleQuotesStringRegExp.source.replace(
+    /<singleQuotesString>/g,
+    '<singleQuotesStringSBKey>'
+  )}\\s*\\](?=\\s*:)`
+);
+
+const doubleQuotesStringSBKeyRegExp = new RegExp(
+  `\\[\\s*${doubleQuotesStringRegExp.source.replace(
+    /<doubleQuotesString>/g,
+    '<doubleQuotesStringSBKey>'
+  )}\\s*\\](?=\\s*:)`
+);
+
+const templateStringSBKeyRegExp = new RegExp(
+  `\\[\\s*${templateStringRegExp.source.replace(
+    /<templateString>/g,
+    '<templateStringSBKey>'
+  )}\\s*\\](?=\\s*:)`
+);
+
+const unquotedPropertyRegExp = /(?<propertyName>\w+)(?:\n|\s)*(?=:)/;
+const singleLineCommentRegExp = /\/\/.*/;
+const multiLineCommentRegExp = /\/\*(?:.|\n)*?\*\//;
+const trailingCommaRegExp = /,(?<trailingBracket>(?:\s|\n)*?(?:}|]))/;
+
+const tokensUnionRegExp = [
+  singleQuotesStringSBKeyRegExp.source,
+  doubleQuotesStringSBKeyRegExp.source,
+  templateStringSBKeyRegExp.source,
+  singleQuotesStringRegExp.source,
+  doubleQuotesStringRegExp.source,
+  templateStringRegExp.source,
+  unquotedPropertyRegExp.source,
+  singleLineCommentRegExp.source,
+  multiLineCommentRegExp.source,
+  trailingCommaRegExp.source,
+].join('|');
+
+const tokensRegExp = new RegExp(`(?:${tokensUnionRegExp})`, 'g');
+
+export function parse(looseJSON: string): ReturnType<typeof JSON.parse> {
+  const normalized = looseJSON
+    .replace(tokensRegExp, match => {
+      // remove all comments first
+      if (match.startsWith('//') || match.startsWith('/*')) {
+        return '';
+      }
+      return match;
+    })
+    .replace(tokensRegExp, (match, ...args) => {
+      const groups = args[args.length - 1] as Record<
+        string,
+        string | undefined
+      >;
+      const singleQuotesString =
+        groups['singleQuotesStringSBKey'] || groups['singleQuotesString'];
+      const doubleQuotesString =
+        groups['doubleQuotesStringSBKey'] || groups['doubleQuotesString'];
+      const templateString =
+        groups['templateStringSBKey'] || groups['templateString'] || '';
+
+      if (singleQuotesString) {
+        return toDoubleQuotes(singleQuotesString);
+      }
+
+      if (doubleQuotesString) {
+        return unescapeSingleQuotes(doubleQuotesString);
+      }
+
+      if (templateString) {
+        return processTemplateString(templateString);
+      }
+
+      if (groups['trailingBracket']) {
+        return groups['trailingBracket'];
+      }
+
+      if (groups['propertyName']) {
+        return `"${groups['propertyName']}"`;
+      }
+
+      return match;
+    });
+
+  console.log(normalized);
+
+  return JSON.parse(normalized);
 }
 
-function singleQuotesToDoubleQuotes(string: string) {
+function unescapeSingleQuotes(string: string) {
+  return string.replace(/\\'/g, "'");
+}
+
+function unescapeBackQuotes(string: string) {
+  return string.replace(/\\`/g, '`');
+}
+
+function unescapeDollar(string: string) {
+  return string.replace(/\\\$/g, '$');
+}
+
+function toDoubleQuotes(string: string) {
   const unquoted = string.substring(1, string.length - 1);
-  const escaped = unescapeDoubleQuotesString(unquoted).replace(
-    /(?<!\\)"/gm,
+  const escaped = unescapeBackQuotes(unescapeSingleQuotes(unquoted)).replace(
+    /(?<!\\)"/g,
     '\\"'
   );
 
   return `"${escaped}"`;
 }
 
-function normalizeJSStrings(string: string) {
-  return string.replace(stringsRegExp, match => {
-    return match[0] === "'"
-      ? singleQuotesToDoubleQuotes(match)
-      : unescapeDoubleQuotesString(match);
-  });
-}
+const templateStringHasInterpolationsRegExp = /(?<!\\)\$\{/g;
+function processTemplateString(string: string) {
+  if (templateStringHasInterpolationsRegExp.test(string)) {
+    return string;
+  }
 
-function removeJSComments(string: string) {
-  return string
-    .replace(multiLineCommentsRegExp, '')
-    .replace(singleLineCommentsRegExp, '');
-}
-
-function removeTrailingCommas(string: string) {
-  return string.replace(trailingCommasRegExp, '$2');
-}
-
-export function jsObjectAsStringToJSON(jsObjectAsString: string): string {
-  const normalized = normalizeJSStrings(
-    removeTrailingCommas(removeJSComments(jsObjectAsString))
-  ).replace(unquotedPropertyRegExp, '"$1":');
-
-  // validate
-  JSON.parse(normalized);
-
-  return normalized;
+  return toDoubleQuotes(unescapeDollar(string.replace(/\n/g, '\\n')));
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,51 +1,240 @@
-import { jsObjectAsStringToJSON } from '../src';
+import { parse } from '../src';
 
 describe('index', () => {
-  describe('jsObjectAsStringToJSON', () => {
-    it('should convert empty object to json', () => {
-      expect(jsObjectAsStringToJSON('{}')).toEqual('{}');
+  describe('parse', () => {
+    it('should parse empty json', () => {
+      expect(parse('{}')).toMatchObject({});
     });
 
-    it('should convert complex js object to json', () => {
+    it('should parse json with any-quoted strings', () => {
       expect(
-        jsObjectAsStringToJSON(`
-{
-  // some single line comment
-  fooBar: 'baz',
-  barBaz: 123,
-  'bar-baz': false,
-  "alex": true,
-  "alex[name=\\"double-escaped\\"]": {
-    someNested: [
-      1,
-      2,/*
-        // inner comment
-      */
-      { "maxWidth": '123px', 'max-height': "value" },
-    ],
-    anotherNested: [1, 2,{}, 'foo', ''],
-    /*
-
-    another: [1, 2, 'foo','bar' {}, "hello"]
-    'multiline comment'
-  */
-    fooBar: [1,2, false, true, 'some-"string"', 3,]
-  },
-  'alex[name=\\'single-escaped\\']': false,//
-  "alex[name=\\'single-escaped\\']": [1,2,false,  ],//
-  "alex[name='single-unescaped']": [1,2,3,false], // : ['some value'],
-  'alex[name=\\"double-escaped\\"]': "some string val'ue",/**/
-'alex[name="double-unescap\\'ed"]': "some string val'\\"u\\"'e",
-  'alex[name="double-unescaped"]': "some string 'value'",
-}
-      `)
-      ).toMatchSnapshot();
+        parse(`{
+          "double \`quoted\` 'string' \\"with \\"escaped quotes \\"": 'single \`string\` "string" \\'with \\'escaped quotes \\'',
+          'single \`string\` "string" \\'with \\'escaped quotes \\'': "double \`quoted\` 'string' \\"with \\"escaped quotes \\"",
+          ["double \`quoted\` 'string' \\"with \\"escaped quotes \\" in square quoted key"]: ['single \`string\` "string" \\'with \\'escaped quotes \\''],
+          ['single \`string\` "string" \\'with \\'escaped quotes \\' in square quoted key']: ["double \`quoted\` 'string' \\"with \\"escaped quotes \\""],
+          [\` "multi-line"
+          \\\${Template string} 'key"
+          \\\` inner escaped ' string
+          '''"
+    \\\` 
+\`]: \`single-line \\\${"Template 'string} value'" \\\` inner escaped string \`,
+          [\`single-line \\\${Template string} key \\\` inner escaped string \`] : \`
+  "multi-line"
+          \\\${Template string} 'value"
+    \\\` inner escaped ' string
+      \\\` 
+      '''"
+\`,
+          "nested\\"-object test": {
+            '': [\`single-line "Template 'string value'" \\\` inner escaped string \`,"double \`quoted\` 'string' \\"with \\"escaped quotes \\"",'single \`string\` "string" \\'with \\'escaped quotes \\''],
+            'multiline "array"' : [
+              \`single-line "Template 'string value'" \\\` inner escaped string \`,
+              "double \`quoted\` 'string' \\"with \\"escaped quotes \\"",
+              'single \`string\` "string" \\'with \\'escaped quotes \\'',
+            ]
+          },
+          [\`\`]: [\`single-line "Template 'string value'" \\\` inner escaped string \`]
+        }`)
+      ).toMatchObject({
+        'double `quoted` \'string\' "with "escaped quotes "':
+          "single `string` \"string\" 'with 'escaped quotes '",
+        "single `string` \"string\" 'with 'escaped quotes '":
+          'double `quoted` \'string\' "with "escaped quotes "',
+        'double `quoted` \'string\' "with "escaped quotes " in square quoted key':
+          ["single `string` \"string\" 'with 'escaped quotes '"],
+        "single `string` \"string\" 'with 'escaped quotes ' in square quoted key":
+          ['double `quoted` \'string\' "with "escaped quotes "'],
+        " \"multi-line\"\n          ${Template string} 'key\"\n          ` inner escaped ' string\n          '''\"\n    ` \n":
+          'single-line ${"Template \'string} value\'" ` inner escaped string ',
+        'single-line ${Template string} key ` inner escaped string ':
+          "\n  \"multi-line\"\n          ${Template string} 'value\"\n    ` inner escaped ' string\n      ` \n      '''\"\n",
+        'nested"-object test': {
+          '': [
+            'single-line "Template \'string value\'" ` inner escaped string ',
+            'double `quoted` \'string\' "with "escaped quotes "',
+            "single `string` \"string\" 'with 'escaped quotes '",
+          ],
+          'multiline "array"': [
+            'single-line "Template \'string value\'" ` inner escaped string ',
+            'double `quoted` \'string\' "with "escaped quotes "',
+            "single `string` \"string\" 'with 'escaped quotes '",
+          ],
+        },
+        '': ['single-line "Template \'string value\'" ` inner escaped string '],
+      });
     });
 
-    it('should throw an error when converting invalid js object to json', () => {
-      expect(() => jsObjectAsStringToJSON('some invalid json')).toThrow(
-        SyntaxError
-      );
+    it('should throw an error when parsing json with template string with interpolations', () => {
+      expect(() => {
+        parse('{[`some ${Template string} value`]: "hello"}');
+      }).toThrow(SyntaxError);
+
+      expect(() => {
+        parse('{[`$some {`Template string` value}`]: "hello"}');
+      }).toThrow(SyntaxError);
+
+      expect(() => {
+        parse('{"hello": `some ${Template string} value`}');
+      }).toThrow(SyntaxError);
+
+      expect(() => {
+        parse('{"hello": `$some {`Template string` value}`}');
+      }).toThrow(SyntaxError);
+    });
+
+    it('should parse json with unquoted properties', () => {
+      expect(
+        parse(`{
+          foo: "bar",
+          foo_bar: 123,
+          foo_123BAr_456_BAZ: 1234,
+          bar1Baz2: "baz bat"
+        }`)
+      ).toMatchObject({
+        foo: 'bar',
+        foo_bar: 123,
+        foo_123BAr_456_BAZ: 1234,
+        bar1Baz2: 'baz bat',
+      });
+    });
+
+    it('should throw an error when parsing json with unquoted properties with invalid characters', () => {
+      expect(() => {
+        parse(`{
+          foo bar: "bar"
+        }`);
+      }).toThrow(SyntaxError);
+
+      expect(() => {
+        parse(`{
+          foo-bar: "bar"
+        }`);
+      }).toThrow(SyntaxError);
+
+      expect(() => {
+        parse(`{
+        тест: "bar"
+      }`);
+      }).toThrow(SyntaxError);
+    });
+
+    it('should parse json with comments', () => {
+      expect(
+        parse(`{
+          "foo": "/* some string */", /*
+          multiline comment
+          some another: \`\`,
+          // single-line comment inside multiline comment
+          /* /* multiline comment inside multiline comment 
+          */ /**/
+          "bar": "// some string" // inline comment
+          /* "foobar": "Baz", */
+        }`)
+      ).toMatchObject({
+        foo: '/* some string */',
+        bar: '// some string',
+      });
+    });
+
+    it('should parse json with trailing commas', () => {
+      expect(
+        parse(`{
+          "some array": [1,2, /* some comment
+          */
+          ],
+          "nested object": {
+            "element": 123,
+            "another": {
+              "another array": [1,2,3,],
+              "some value": false, // 
+            },
+            "nested array": [1,2,[3,{"some":"value[1,2,3,]{1,'23',}",},4,[5,6,/* some comment */],],],
+          },
+        }`)
+      ).toMatchObject({
+        'some array': [1, 2],
+        'nested object': {
+          element: 123,
+          'nested array': [
+            1,
+            2,
+            [
+              3,
+              {
+                some: "value[1,2,3,]{1,'23',}",
+              },
+              4,
+              [5, 6],
+            ],
+          ],
+        },
+      });
+    });
+
+    it('should parse json with all tollerances(single quote strings, template strings, unquoted properties, comments and trailing commas)', () => {
+      expect(
+        parse(`{
+          "double \`quoted\` 'string' \\"with \\"escaped quotes \\"": 'single \`string\` "string" \\'with {1,"23",} \\'escaped quotes \\'',
+          'single \`string\` "string" \\'with \\'escaped quotes \\'': "double \`quoted\` 'string' \\"with \\"escaped quotes \\" [1,2,3,]",
+          ["double \`quoted\` 'string' \\"with \\"escaped quotes \\" in square quoted key"]: [
+            {
+              foo_123BAr_456_BAZ: 'single \`string\` "string" /* \\'with \\'escaped quotes */ \\'',
+              [/**/'single \`string\` "string" \\'with \\'escaped quotes \\' in // square quoted key']: ['single \`string\` "string" \\'with \\'escaped quotes \\'', "double \`quoted\` 'string' \\"with \\"escaped quotes \\"",], // some comment
+            },
+            1234,
+            {
+              fooBar: "double \`quoted\` 'string' \\"with \\"escaped quotes \\"",
+              "/* some string */": false,
+              qwerty1234: "/* some string */", /*
+               some multiline comment
+              */
+            }, // testing comment
+          ],
+          [\` "multi-line"
+          \\\${Template string} 'key"
+          \\\` inner escaped ' string
+          '''"
+    \\\` 
+\`]: \`single-line \\\${"Template 'string} value'" \\\` inner escaped string \`,
+          [\`single-line \\\${Template string} key \\\` inner escaped string \`/* some comment */] : \`
+  "multi-line"
+          \\\${Template string} 'value"
+    \\\` inner escaped ' string
+      \\\` 
+      '''"
+\`,
+          "nested\\"-object test {1,'23',}": {
+            '': [\`single-line "Template 'string value'" \\\` inner escaped string \`,"double \`quoted\` 'string' \\"with \\"escaped quotes \\"",'single \`string\` "string" \\'with \\'escaped quotes \\'' //some comment ]
+          ],
+            'multiline "array"' : [/*
+              multiline comment
+              some another: \`\`,
+              // single-line comment inside multiline comment [1,2,3,]
+              /* /* multiline comment inside multiline comment {1,'23',}
+              */ /**/
+              \`single-line "Template 'string value'" \\\` inner escaped string \`, // some single line comment
+              "double \`quoted\` 'string' \\"with \\"escaped quotes \\"", /*
+                some multi-line comment
+              */
+              'single \`string\` "string" \\'with \\'escaped quotes \\'',
+            ]
+          },
+          [\`\`]: [\`single-line "Template 'string value'" \\\` inner escaped string \`]
+        }`)
+      ).toMatchObject({});
+    });
+
+    it('should throw an error when parsing empty string', () => {
+      expect(() => {
+        parse('');
+      }).toThrow(SyntaxError);
+    });
+
+    it('should throw an error when parsing invalid json', () => {
+      expect(() => {
+        parse('some invalid json');
+      }).toThrow(SyntaxError);
     });
   });
 });
